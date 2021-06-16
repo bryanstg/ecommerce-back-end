c6d5f8ea948f1308e3b33592c205c14833c69994
Bryan García <bryan1997st@gmail.com>
2021-06-16 00:57:38 -0500

    Se agregó admin a la vista de admin

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Buyer, Category, Store
+from models import db, User, Buyer, Category, Store, Product
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -17,6 +17,8 @@ def setup_admin(app):
     admin.add_view(ModelView(Category, db.session))
 
     admin.add_view(ModelView(Store, db.session))
+
+    admin.add_view(ModelView(Product, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
